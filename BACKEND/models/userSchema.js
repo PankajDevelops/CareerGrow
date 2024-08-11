297f76d2874193b742cf26527d313766a72e343e
PankajDevelops <npankaj5538@gmail.com>
2024-08-11 17:40:24 +0530

    updation

--- BACKEND/models/userSchema.js
+++ BACKEND/models/userSchema.js
@@ -36,3 +36,23 @@ const userSchema = new mongoose.Schema({
   },
 });
 
+
+//hashing the pass
+userSchema.pre("save", async function(next){
+    if(this.isModified("password")){
+        next()
+    }
+    this.password = await bcrypt.hash(this.password,10);
+})
+
+//comparing pass
+userSchema.methods.comparePassword = async function(enteredPassword){
+  return await bcrypt.compare(enteredPassword,this.password);
+};
+
+//jwt generation for aurthorix=zation
+userSchema.methods.getJWTToken = function(){
+  return jwt.sign({ id: this._id }, process.env.JWT_SECRET_KEY, {
+    expiresIn: process.env.JWT_EXPIRE,
+  });
+}
