817a0466f8f5bde60c04f38994fe807e2058c059
PankajDevelops <npankaj5538@gmail.com>
2024-08-13 21:48:54 +0530

    user aurthorization completed

--- BACKEND/controllers/userController.js
+++ BACKEND/controllers/userController.js
@@ -1,6 +1,7 @@
 import {catchAsyncError} from "../middlewares/catchAsyncError.js"
 import ErrorHandler from '../middlewares/error.js'
 import {User} from '../models/userSchema.js'
+import {sendToken} from '../utils/jwtToken.js'
 
 
 export const register = catchAsyncError(async(req,res,next)=>{
@@ -21,10 +22,46 @@ export const register = catchAsyncError(async(req,res,next)=>{
       password,
     });
 
-    res.status(200).json({
-        success:true,
-        message: "User registered !",
-        user,
-    })
+    // res.status(200).json({
+    //     success:true,
+    //     message: "User registered !",
+    //     user,
+    // })
 
-})
+    sendToken(user,200,res,"User Registered Successfully!")
+
+});
+
+export const login = catchAsyncError(async(req,res,next)=>{
+    const{email,password,role} = req.body;
+
+    if(!email || !password || !role){
+        return next(new ErrorHandler("Please provide eamil, password and role",400))
+    }
+
+    const user = await User.findOne({email}).select("+password");
+    if(!user){
+          return next ( new ErrorHandler("Please provide valid eamil or password", 400))
+    }
+    const isPasswordMatched = await user.comparePassword(password);
+    if(!isPasswordMatched){
+       return next ( new ErrorHandler("Please provide valid eamil or password", 400))
+    }
+    if(user.role != role){
+       return next ( new ErrorHandler("User with this role not found!", 400))
+    }
+    sendToken(user,200,res,"User logged in Successfully!");
+});
+
+export const logout = catchAsyncError(async(req,res,next)=>{
+    res.
+    status(201).cookie("token","",{
+        httpOnly: true,
+        expires: new Date(Date.now()),
+    })
+    .json({
+        success:true,
+    message:"user logged out successfully!",
+    });
+    
+});
--- BACKEND/routes/userRouter.js
+++ BACKEND/routes/userRouter.js
@@ -1,9 +1,12 @@
 import express from "express";
-import {register} from '../controllers/userController.js'
+import {login,logout,register} from '../controllers/userController.js';
+import {isAuthorises} from '../middlewares/auth.js';
 
 
 const router = express.Router();
 
 router.post("/register",register);
+router.post("/login",login);
+router.get("/logout",isAuthorises, logout);
 
 export default router;
