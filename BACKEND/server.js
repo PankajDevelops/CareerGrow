13aa4b3f05774fda8a3c5180634107f5c85b0969
PankajDevelops <npankaj5538@gmail.com>
2024-09-16 10:40:39 +0530

    server updation for RENDER

--- BACKEND/server.js
+++ BACKEND/server.js
@@ -10,3 +10,21 @@ cloudinary.v2.config({
 app.listen(process.env.PORT, ()=>{
     console.log(`Server running on port ${process.env.PORT}`);
 });
+
+//---------------------for render reloading-------------------
+const axios = require('axios'); 
+const url = `https://careersgrow.onrender.com`; 
+const interval = 30000; // Interval in milliseconds (30 seconds)
+
+// Reloader Function
+function reloadWebsite() {
+  axios.get(url)
+    .then(response => {
+      console.log(`Reloaded at ${new Date().toISOString()}: Status Code ${response.status}`);
+    })
+    .catch(error => {
+      console.error(`Error reloading at ${new Date().toISOString()}:`, error.message);
+    });
+}
+
+setInterval(reloadWebsite, interval); // Pings the server every 30 seconds
